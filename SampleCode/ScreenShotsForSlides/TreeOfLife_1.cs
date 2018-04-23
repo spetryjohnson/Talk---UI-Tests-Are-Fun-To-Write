58b4400d90c277e5d67ee5f5685915fd74a92589
Seth Petry-Johnson <seth@petry-johnson.com>
2018-04-22 23:03:59 -0400

    More polish

---
+++ SampleCode/ScreenShotsForSlides/TreeOfLife_1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using AdvancedUITesting.SeleniumTests;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace AdvancedUITesting.ScreenShotsForSlides.TreeOfLife.Slide1 {
+
+	public class LoginPage {
+
+		[FindsBy(How = How.Id, Using = "CycleExpiredFlag")]
+		public IWebElement CycleExpired { get; protected set; }
+	}
+}
+
---
+++ SampleCode/ScreenShotsForSlides/TreeOfLife_2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using AdvancedUITesting.SeleniumTests;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace AdvancedUITesting.ScreenShotsForSlides.TreeOfLife.Slide2 {
+
+	public class LoginPage {
+
+		[FindsBy(How = How.Id, Using = "table.Foo > tr > td > a")]
+		public IWebElement Foo { get; protected set; }
+	}
+}
+
