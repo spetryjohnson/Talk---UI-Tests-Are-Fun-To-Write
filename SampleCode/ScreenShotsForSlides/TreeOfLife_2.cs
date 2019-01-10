c34ac5be4ff8d43471d655a6f67726145834b216
Seth Petry-Johnson <seth@petry-johnson.com>
2019-01-10 16:21:15 -0500

    Slide and narrative tweaks - streamlining

--- SampleCode/ScreenShotsForSlides/TreeOfLife_2.cs
+++ SampleCode/ScreenShotsForSlides/TreeOfLife_2.cs
@@ -10,7 +10,12 @@ namespace AdvancedUITesting.ScreenShotsForSlides.TreeOfLife.Slide2 {
 
 	public class LoginPage {
 
-		[FindsBy(How = How.CssSelector, Using = "#Foo > tr > td > a")]
+		[FindsBy(How = How.CssSelector, Using = "#Foo tr td > span[0] > a")]
+		public IWebElement Foo { get; protected set; }
+	}
+	public class LoginPage2 {
+
+		[FindsBy(How = How.Id, Using = "#Selenium-FooLink")]
 		public IWebElement Foo { get; protected set; }
 	}
 }
